af3818fd426db9128cbd6d1da927ca06dcac4a26
Crunchy77 <holyheart028@gmail.com>
2022-09-08 23:18:16 -0700

    Final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main return 0
+ *main - main block
+ *Return: 0
  */
 int main(void)
 {
-puts("\"Programming is loke building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
