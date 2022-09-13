a9f8fa5d1df4b09cfb8d49b3fb2f09ef82f9991a
Crunchy77 <holyheart028@gmail.com>
2022-09-12 22:29:12 -0700

    Add

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: Work
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a';x <= 'z'; x++)
+	{
+		putchar(x);
+		if (x == 'z')
+		{
+			putchar('\n');
+		}
+
+	}
+
+
+	return 0;
+}
