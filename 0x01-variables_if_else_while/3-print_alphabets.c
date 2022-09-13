fd36087581eb7ee404a7c9d3d4ca750854e80830
Crunchy77 <holyheart028@gmail.com>
2022-09-12 23:01:11 -0700

    Push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Work
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x,y;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+		
+	}
+
+	for (y = 'A' ; y <= 'Z' ; y++)
+	{
+		putchar(y);
+
+		if (y == 'Z')
+		{
+			putchar('\n');
+		}
+	}
+	
+	return (0);
+}
