897cca9a3ab4d3f597e1c78bcbcf58ad5061517c
Crunchy77 <holyheart028@gmail.com>
2022-09-12 23:48:43 -0700

    Add

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,14 +4,14 @@
 /**
  * main - main block
  * Description: Work
- * return: 0
+ * Return: 0
  */
 
 int main(void)
 {
 	int x;
 
-	for(x = 0 ; x <10 ; x++)
+	for(x = 0 ; x < 10 ; x++)
 	{
 		printf("%d", x);
 		if (x ==  9)
