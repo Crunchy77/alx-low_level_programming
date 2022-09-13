f9afc65dd01ebb31386377bee8bbec727d7e114f
Crunchy77 <holyheart028@gmail.com>
2022-09-13 00:30:44 -0700

    Add

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+ /**
+  * main - main block
+  * Description: sth
+  * Return: 0
+  */
+
+int main(void)
+{
+	int x;
+
+	for(x = 'z' ; x >= 'a' ; x--)
+	{
+		putchar(x);
+		if (x == 'a')
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
