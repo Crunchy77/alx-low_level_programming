72a11acba2ed9dc1b4c1ad19ce9b69be4f22e8f9
Crunchy77 <holyheart028@gmail.com>
2022-09-13 00:38:34 -0700

    Add

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
- /**
-  * main - main block
-  * Description: sth
-  * Return: 0
-  */
+/**
+ * main - main block
+ * Description: sth
+ * Return: 0
+ */
 
 int main(void)
 {
