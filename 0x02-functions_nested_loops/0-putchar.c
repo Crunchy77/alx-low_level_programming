9c2c3f72af3a84c4d01e9d765b936133a4da13f8
Crunchy77 <holyheart028@gmail.com>
2022-09-13 22:55:30 -0700

    First

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: Work
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[10] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
