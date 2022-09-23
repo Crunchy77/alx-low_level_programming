2ebcc2e44c73a45e9498c6889fe2f69833436e61
Crunchy77 <holyheart028@gmail.com>
2022-09-23 14:59:23 -0700

    late

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- **_strcat - concatenates two strings
- *@dest: pointer destination
- *@src: pointer source
- *Return: void
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ *
+ * Description - concatenation
+ * Return: char
  */
 
 char *_strcat(char *dest, char *src)
